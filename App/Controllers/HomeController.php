08b6c732f56b5384c8b4d494613b17590ffc5e29
Gitixig <hatuandat190290@gmail.com>
2025-04-23 11:13:52 +0700

    shopping cart

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -1,8 +1,10 @@
 <?php
+require_once __DIR__ . "/../Model/ProductModel.php";
 class HomeController
 {
     public function index()
-    {
+    {   $Product=new ProductModel();
+        $productlist=$Product->getAllProducts();
         include __DIR__ . '/../Views/Home/index.php';
     }
     
